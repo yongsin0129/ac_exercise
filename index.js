248c62efe2037e2bc34ac6d4e2b4dca750c3af93
yongsin syu <yongsin0129@gmail.com>
2022-04-05 19:13:05 +0800

    initial

---
+++ index.js
@@ -0,0 +1,65 @@
+const BASE_URL = 'https://movie-list.alphacamp.io'
+const INDEX_URL = BASE_URL + '/api/v1/movies/'
+const POSTER_URL = BASE_URL + '/posters/'
+const movies = []
+const dataPanel = document.querySelector('#data-panel')
+const searchForm = document.querySelector('#search-form')
+
+
+axios
+  .get(INDEX_URL)
+  .then((response) => {
+    movies.push(...response.data.results)
+    renderMovieList(movies)
+  })
+  .catch((err) => console.log(err))
+
+// 監聽 data panel 點擊 more 會跑出 movie 細節
+dataPanel.addEventListener('click', (event) => {
+  if (event.target.matches('.btn-show-movie')) {
+    showMovieModal(event.target.dataset.id)  // 修改這裡
+  }
+})
+
+// 監聽 search bar 
+searchForm.addEventListener('submit', function onSearchFormSubmitted (event) {
+  event.preventDefault()
+  console.log(event)
+})
+
+function renderMovieList (data) {
+  let rawHTML = ''
+  data.forEach((item) => {
+    // title, image
+    rawHTML += `<div class="col-sm-3">
+    <div class="mb-2">
+      <div class="card">
+        <img src="${POSTER_URL + item.image}" class="card-img-top" alt="Movie Poster">
+        <div class="card-body">
+          <h5 class="card-title">${item.title}</h5>
+        </div>
+        <div class="card-footer">
+          <button class="btn btn-primary btn-show-movie" data-bs-toggle="modal" data-bs-target="#movie-modal" data-id="${item.id}">More</button>
+          <button class="btn btn-info btn-add-favorite">+</button>
+        </div>
+      </div>
+    </div>
+  </div>`
+  })
+  dataPanel.innerHTML = rawHTML
+}
+
+function showMovieModal (id) {
+  const modalTitle = document.querySelector('#movie-modal-title')
+  const modalImage = document.querySelector('#movie-modal-image')
+  const modalDate = document.querySelector('#movie-modal-date')
+  const modalDescription = document.querySelector('#movie-modal-description')
+  axios.get(INDEX_URL + id).then((response) => {
+    const data = response.data.results
+    modalTitle.innerText = data.title
+    modalDate.innerText = 'Release date: ' + data.release_date
+    modalDescription.innerText = data.description
+    modalImage.innerHTML = `<img src="${POSTER_URL + data.image
+      }" alt="movie-poster" class="img-fluid">`
+  })
+}
